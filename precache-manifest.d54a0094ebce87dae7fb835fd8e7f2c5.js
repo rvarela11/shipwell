e6244c9a5ceb449aaaa80676a00792b0238d74e2
Robert Varela <rvarela@utexas.edu>
2019-03-08 22:16:15 -0600

    Updates

---
+++ precache-manifest.d54a0094ebce87dae7fb835fd8e7f2c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86d57f321aaf231be231c199d6e74986",
+    "url": "/shipwell/static/media/shipwell-logo.86d57f32.png"
+  },
+  {
+    "revision": "637c2865f979bda122e8",
+    "url": "/shipwell/static/js/runtime~main.637c2865.js"
+  },
+  {
+    "revision": "d64f6ad8e6ed8fd38633",
+    "url": "/shipwell/static/js/main.d64f6ad8.chunk.js"
+  },
+  {
+    "revision": "99a7af0a6ec1640920d0",
+    "url": "/shipwell/static/js/2.99a7af0a.chunk.js"
+  },
+  {
+    "revision": "d64f6ad8e6ed8fd38633",
+    "url": "/shipwell/static/css/main.5bb9a4e1.chunk.css"
+  },
+  {
+    "revision": "7f52a3fed5807b562eb04374cb70c402",
+    "url": "/shipwell/index.html"
+  }
+];
