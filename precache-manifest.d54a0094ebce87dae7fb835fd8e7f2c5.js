c2a7d00bed3a2c21ebb7378414f108abc8d3fe53
Robert Varela <rvarela@utexas.edu>
2019-03-08 22:20:48 -0600

    Updates

--- precache-manifest.d54a0094ebce87dae7fb835fd8e7f2c5.js
+++ precache-manifest.d54a0094ebce87dae7fb835fd8e7f2c5.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/shipwell/static/js/runtime~main.637c2865.js"
   },
   {
-    "revision": "53b456f9f7b146135aba",
-    "url": "/shipwell/static/js/main.53b456f9.chunk.js"
+    "revision": "d64f6ad8e6ed8fd38633",
+    "url": "/shipwell/static/js/main.d64f6ad8.chunk.js"
   },
   {
     "revision": "99a7af0a6ec1640920d0",
     "url": "/shipwell/static/js/2.99a7af0a.chunk.js"
   },
   {
-    "revision": "53b456f9f7b146135aba",
+    "revision": "d64f6ad8e6ed8fd38633",
     "url": "/shipwell/static/css/main.5bb9a4e1.chunk.css"
   },
   {
-    "revision": "39195a4fcd08e2c81217233974de662c",
+    "revision": "7f52a3fed5807b562eb04374cb70c402",
     "url": "/shipwell/index.html"
   }
 ];
